085873faaeea672078a0a3a5a0c90ee52a66b40b
bee <114894060+bee85919@users.noreply.github.com>
2023-07-31 23:40:42 -0700

    [level 3] Title: 등굣길, Time: 2.28 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -0,0 +1,11 @@
+def solution(m, n, pd):
+    
+    pd, dp, dp[1][1] = [[q,p] for [p,q] in pd], [[0] * (m + 1) for i in range(n + 1)], 1
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i==1 and j==1: continue 
+            if [i,j] in pd: dp[i][j] = 0
+            else: dp[i][j] = (dp[i-1][j] + dp[i][j-1])
+            
+    return dp[n][m] % 1000000007
