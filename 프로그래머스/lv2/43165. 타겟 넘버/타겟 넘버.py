a76c64570ed6800f93fcb7e45f919309bb152a72
bee <114894060+bee85919@users.noreply.github.com>
2023-08-04 23:22:53 +0900

    [level 2] Title: 타겟 넘버, Time: 3.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,1 +1,2 @@
-def solution(num, n): return 1 if not num and n == 0 else 0 if not num else solution(num[1:], n-num[0]) + solution(num[1:], n+num[0])
+def solution(num, n): 
+		return 1 if not num and n == 0 else 0 if not num else solution(num[1:],n-num[0]) + solution(num[1:],n+num[0])
