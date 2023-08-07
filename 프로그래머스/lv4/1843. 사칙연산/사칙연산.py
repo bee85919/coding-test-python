d440bb3d19ae302a03834e968ba4e399fc43b831
bee <114894060+bee85919@users.noreply.github.com>
2023-08-07 16:46:23 +0900

    [level 4] Title: 사칙연산, Time: 123.19 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/lv4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/lv4/1843. 사칙연산/사칙연산.py
@@ -1,20 +1,17 @@
 def cal(a,b,op): return a+b if op=="+" else a-b
 
+
 def solution(arr):    
-    
+
     M, m, num, ops = {}, {}, [int(x) for x in arr[::2]], [x for x in arr[1::2]]
-    
+
     for i in range(len(num)): M[(i,i)], m[(i,i)] = num[i], num[i]
     
     for d in range(1,len(num)):
-        for i in range(len(num)):
-            if i+d >= len(num): continue
+        for i in range(len(num)-d):  
             
-            M_list, m_list = [], []
-            for k in range(i+1, i+1+d):
-                M_list.append(cal(M[(i,k-1)], m[(k,i+d)], ops[k-1]))
-                m_list.append(cal(m[(i,k-1)], M[(k,i+d)], ops[k-1]))
-            
-            M[(i,i+d)], m[(i,i+d)] = max(M_list), min(m_list)
+            M_list, m_list = [cal(M[(i,k)], m[(k+1, i+d)], ops[k]) for k in range(i, i+d)], [cal(m[(i,k)], M[(k+1, i+d)], ops[k]) for k in range(i, i+d)]
+                
+            M[(i, i+d)], m[(i, i+d)] = max(M_list), min(m_list)
             
     return M[(0,len(num)-1)]
