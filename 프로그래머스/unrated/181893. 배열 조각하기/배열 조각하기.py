cfa195d86ee2685132a2fdfc70679966b485af1e
bee <114894060+bee85919@users.noreply.github.com>
2023-08-14 23:44:35 +0900

    [unrated] Title: 배열 조각하기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.py
@@ -1,7 +1,7 @@
 def solution(arr, query):
-    for i, q in enumerate(query):
+    for i, que in enumerate(query):
         if i%2 == 0: 
-            arr = arr[:q+1]
+            arr = arr[:que+1]
         else: 
-            arr = arr[q:]
+            arr = arr[que:]
     return arr
