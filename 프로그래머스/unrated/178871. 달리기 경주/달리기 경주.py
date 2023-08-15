10a827d781a129c34f61d16baf9b438b3c231d53
bee <114894060+bee85919@users.noreply.github.com>
2023-08-15 23:59:30 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,8 @@
+def solution(players, calls):    
+    player_dict = {player: rank for rank, player in enumerate(players)}
+    rank_dict = {rank: player for rank, player in enumerate(players)}    
+    for call in calls:
+        rank = player_dict[call]        
+        player_dict[rank_dict[rank-1]], player_dict[rank_dict[rank]] = player_dict[rank_dict[rank]], player_dict[rank_dict[rank-1]]
+        rank_dict[rank-1], rank_dict[rank] = rank_dict[rank], rank_dict[rank-1]    
+    return list(rank_dict.values())
