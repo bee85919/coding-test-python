df5eaaa84901b402f81b101973af365eb57d1cb2
bee <114894060+bee85919@users.noreply.github.com>
2023-08-18 13:55:47 +0900

    [level 3] Title: 아이템 줍기, Time: 0.59 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,35 +1,39 @@
 from collections import deque
 
 def solution(rect, charX, charY, itemX, itemY):
+    answer = 0
+    field = [[-1] * 102 for _ in range(102)]
     
-    ans = 0
-
-    field = [[-1] * 102 for i in range(102)]
-    
+    # Double all coordinates and fill the field
     for r in rect:
-    	# 모든 좌표값 2배
-        x1, y1, x2, y2 = map(lambda x: x*2, r)
-        for i in range(x1, x2+1):
-            for j in range(y1, y2+1):
-                if x1 < i < x2 and y1 < j < y2: field[i][j] = 0
-                elif field[i][j] != 0: field[i][j] = 1
+        x1, y1, x2, y2 = map(lambda x: x * 2, r)
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                if x1 < i < x2 and y1 < j < y2:
+                    field[i][j] = 0
+                elif field[i][j] == -1: # Fill with 1 for border
+                    field[i][j] = 1
                     
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    
     q = deque()
     q.append([charX * 2, charY * 2])
-    visited = [[1] * 102 for i in range(102)]
+    visited = [[1] * 102 for _ in range(102)]
     visited[charX * 2][charY * 2] = 0
     
     while q:
-        x, y = q.popleft()        
-        if x == itemX * 2 and y == itemY * 2: ans = visited[x][y] // 2; break
+        x, y = q.popleft()
+        if x == itemX * 2 and y == itemY * 2:
+            answer = visited[x][y] // 2
+            break
+
         for k in range(4):
             nx = x + dx[k]
             ny = y + dy[k]
-            if field[nx][ny] == 1 and visited[nx][ny] == 1:
+            
+            # Check if the next position is within the field and is a border
+            if 0 <= nx < 102 and 0 <= ny < 102 and field[nx][ny] == 1 and visited[nx][ny] == 1:
                 q.append([nx, ny])
                 visited[nx][ny] = visited[x][y] + 1
     
-    return ans
+    return answer
