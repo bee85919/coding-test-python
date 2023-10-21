e4d5dac7a85d72d872b16558a99301792b6e1ee8
bee <114894060+bee85919@users.noreply.github.com>
2023-10-21 17:43:00 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -5,13 +5,8 @@ n, stack = int(sys.stdin.readline()), []
 for _ in range(n):
     cmd = sys.stdin.readline().split()
 
-    if cmd[0] == 'push':
-        stack.append(cmd[1])
-    elif cmd[0] == 'pop':
-        print(stack.pop() if stack else -1)
-    elif cmd[0] == 'size':
-        print(len(stack))
-    elif cmd[0] == 'empty':
-        print(1 if not stack else 0)
-    elif cmd[0] == 'top':
-        print(stack[-1] if stack else -1)
+    if   cmd[0] == 'push'  : stack.append(cmd[1])
+    elif cmd[0] == 'pop'   : print(stack.pop() if stack else -1)
+    elif cmd[0] == 'size'  : print(len(stack))
+    elif cmd[0] == 'empty' : print(1 if not stack else 0)
+    elif cmd[0] == 'top'   : print(stack[-1] if stack else -1)
