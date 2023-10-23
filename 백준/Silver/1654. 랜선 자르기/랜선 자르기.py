602db262cc3e086c089e27d589989b697b5fa585
bee <114894060+bee85919@users.noreply.github.com>
2023-10-23 14:37:41 +0900

    [Silver II] Title: 랜선 자르기, Time: 64 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,13 +1,17 @@
-K, N = map(int, input().split())
-L = [int(input()) for _ in range(K)]
+import sys
+input = sys.stdin.readline
 
-S, E = 1, max(L)
-while S <= E:
-    M = (S+E) // 2
-    T = sum(l // M for l in L)
-    if T < N:
-        E = M - 1
-    else:
-        S = M + 1
 
-print(E)
+def get_mid(shrt, long): return (shrt + long) // 2
+def cnt_lan(lans, mid): return sum(l // mid for l in lans)
+def cal_mid(long, shrt, cnt, mid, n): return (mid-1, shrt) if cnt<n else (long, mid+1)
+
+
+k, n = map(int, input().split())
+lans = [int(input()) for _ in range(k)]
+shrt, long = 1, max(lans)
+while shrt <= long:
+    mid = get_mid(shrt, long)
+    cnt = cnt_lan(lans, mid)
+    long, shrt = cal_mid(long, shrt, cnt, mid, n)
+print(long)
