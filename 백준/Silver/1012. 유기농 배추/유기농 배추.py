aeb6f0c2e94040f0c3ad1bb0d9a3357925f24991
bee <114894060+bee85919@users.noreply.github.com>
2023-10-27 00:53:15 +0900

    [Silver II] Title: 유기농 배추, Time: 52 ms, Memory: 31644 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -7,32 +7,32 @@ dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
 
 
 def dfs(x, y, field, visit, m, n):
-    visit[y][x] = True
+    visit[x][y] = True
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
         if 0 <= nx < m and 0 <= ny < n:
-            if field[ny][nx] == 1 and not visit[ny][nx]:
+            if field[nx][ny] == 1 and not visit[nx][ny]:
                 dfs(nx, ny, field, visit, m, n)
 
 
 def create_field_and_visit(m, n):
-    field = [[0] * m for _ in range(n)]
-    visit = [[False] * m for _ in range(n)]
+    field = [[0] * n for _ in range(m)]
+    visit = [[False] * n for _ in range(m)]
     return field, visit
 
 
 def fill_field(k, field):
     for _ in range(k):
         x, y = map(int, input().split())
-        field[y][x] = 1
+        field[x][y] = 1
     return field
 
 
-def count(m, n, field, visit):
+def cnt(m, n, field, visit):    
     count = 0
     for i in range(m):
         for j in range(n):
-            if field[j][i] == 1 and not visit[j][i]:
+            if field[i][j] == 1 and not visit[i][j]:
                 dfs(i, j, field, visit, m, n)
                 count += 1
     return count
@@ -43,4 +43,4 @@ for _ in range(t):
     m, n, k = map(int, input().split())
     field, visit = create_field_and_visit(m, n)
     field = fill_field(k, field)
-    print(count(m, n, field, visit))
+    print(cnt(m, n, field, visit))
