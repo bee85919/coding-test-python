47da282a5bb212aac78acdd0722e9c26a420cff7
bee <114894060+bee85919@users.noreply.github.com>
2023-11-25 22:55:31 +0900

    [Silver III] Title: 수열, Time: 92 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,12 +1,21 @@
+def get_pre_sum():
+    s = 0
+    pre_sum = [0]*(n+1)
+    for i in range(n):
+        s += arr[i]
+        pre_sum[i+1] = s
+    return pre_sum
 
-n, k = map(int, input().split())
-array = list(map(int, input().split()))
-psum = [0] * (n+1)
-s = 0
-for i in range(n):
-    s += array[i]
-    psum[i+1] = s
-ans = -int(1e9)
-for i in range(k, n+1):
-    ans = max(ans, psum[i]-psum[i-k])
-print(ans)
+
+def get_max_sum():
+    max_sum = -int(1e9)
+    for i in range(k, n+1):
+        max_sum = max(max_sum, pre_sum[i]-pre_sum[i-k])
+    return max_sum
+
+
+n,k = map(int, input().split())
+arr = list(map(int, input().split()))
+pre_sum = get_pre_sum()
+max_sum = get_max_sum()
+print(max_sum)
