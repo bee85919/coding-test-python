f9b7a0c21351c53c6b10617428301649f55c743e
bee <114894060+bee85919@users.noreply.github.com>
2023-12-28 00:00:36 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,5 +1,3 @@
 def solution(a, b, c):
     n = len(set([a, b, c]))
-    return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3) if n == 1 \
-    else (a+b+c)*(a**2+b**2+c**2) if n == 2 \
-    else (a+b+c)
+    return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3) if n==1 else (a+b+c)*(a**2+b**2+c**2) if n==2 else (a+b+c)
