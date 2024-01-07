5522c970059b803cb708b0ae3e9c13fece7b6dc9
bee <114894060+bee85919@users.noreply.github.com>
2024-01-08 01:30:34 +0900

    [Silver V] Title: 숫자 카드, Time: 684 ms, Memory: 121536 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,16 +1,11 @@
 n = int(input())
-card = list(map(int, input().split(" ")))
+card = set(map(int, input().split(" ")))
 m = int(input())
 nums = list(map(int, input().split(" ")))
 
 
-di = {}
-for i in range(n):
-    di[card[i]] = 0
-
-    
 for j in range(m):
-    if nums[j] not in di:
+    if nums[j] not in card:
         print(0, end = ' ')
     else:
         print(1, end = ' ')
