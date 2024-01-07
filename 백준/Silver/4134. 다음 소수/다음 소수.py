98b46b997cd4a7074dd8a46465de8b404d081755
bee <114894060+bee85919@users.noreply.github.com>
2024-01-08 02:07:14 +0900

    [Silver IV] Title: 다음 소수, Time: 1484 ms, Memory: 31988 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.py
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -2,11 +2,20 @@ n = int(input())
 num = [int(input()) for _ in range(n)]
 
 
-def check(n):
+def ch(n):
     for i in range(2, int(n**0.5)+1):
         if n%i == 0:
             return False
     return True
+
+
+di = {}
+def dp(n, di):
+    if n in di:
+        return di[n]
+    ch_n = ch(n)
+    di[n] = ch_n
+    return ch_n
 
 
 for n in num:
@@ -14,7 +23,7 @@ for n in num:
         if n == 0 or n == 1:
             print(2)
             break
-        elif check(n):
+        elif dp(n, di):
             print(n)
             break
         else:
