5d2e1fd1c7e28c5b54c86a8871c7fcc33db1d4cb
bee <114894060+bee85919@users.noreply.github.com>
2024-01-17 00:04:16 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,6 +1,6 @@
-def solution(num_list):
-    if int(num_list[-1]) > int(num_list[-2]):
-        num_list.append(int(num_list[-1]) - int(num_list[-2]))
-    else:
-        num_list.append(2*num_list[-1])
-    return num_list
+def solution(li):
+    num1 = int(li[-1])
+    num2 = int(li[-2])
+    num = num1 - num2 if num1 > num2 else num1 * 2
+    li.append(num)
+    return li
